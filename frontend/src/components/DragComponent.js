0aefbbf749c911091cc00267956f7b3ffaa1abc7
oshigoto46 <ppp.shiro46@gmail.com>
2021-09-11 13:55:17 +0000

    strike through

--- frontend/src/components/DragComponent.js
+++ frontend/src/components/DragComponent.js
@@ -20,6 +20,7 @@ export default function DragComponent(props) {
         props.deleteTodo(id)
     }
     const toggleTodoCompleted = (id) => {
+        //alert("toggle")
         props.toggleTodoCompleted(id)
     }
     return(
@@ -28,26 +29,27 @@ export default function DragComponent(props) {
             <Table ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
                   <TableHead>
                       <TableRow>
-                        <TableCell>Status</TableCell>
+                        <TableCell>Status   </TableCell>
                         <TableCell>Task Name</TableCell>
                         <TableCell>Due  Date</TableCell>
-                        <TableCell>finish</TableCell>
+                        <TableCell>Delete   </TableCell>
                        </TableRow>
                   </TableHead>
              
                     <TableBody  display="flex"
                                 flexDirection="row"
                                 alignItems="center"
-                                className={classes.todoContainer}>
-                                    
+                                // className={classes.todoContainer}
+                                >
+
                             <TableCell>
                                 <Checkbox
                                     checked={elem.completed}
                                     onChange={() => toggleTodoCompleted(elem.id)}
                                 ></Checkbox>
                             </TableCell>
-                            <TableCell>{elem.todoText}</TableCell> 
-                            <TableCell>{elem.dueDate}</TableCell> 
+                            <TableCell className={elem.completed ? classes.todoTextCompleted : ""}>{elem.todoText}</TableCell> 
+                            <TableCell className={elem.completed ? classes.todoTextCompleted : ""}>{elem.dueDate}</TableCell> 
                             <Button
                                 // className={classes.deleteTodo}
                                 // startIcon={<Icon>delete</Icon>}

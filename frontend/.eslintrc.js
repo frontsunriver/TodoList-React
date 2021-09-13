2608881ac7acf78b2f351d11c96595352cb72360
sunriver <neymarjohn215@gmail.com>
2021-09-13 12:54:48 +0300

    update main page design

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   extends: [
-    "plugin:prettier/recommended",
-    "prettier",
-    "prettier/react",
-    "react-app",
-    "react-app/jest",
+    // "plugin:prettier/recommended",
+    // "prettier",
+    // "prettier/react",
+    // "react-app",
+    // "react-app/jest",
   ],
 };
